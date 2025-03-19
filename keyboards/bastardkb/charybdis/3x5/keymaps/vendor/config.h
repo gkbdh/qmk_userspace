01696c215b065134cea2f6a188938b577a57f9bf
gkbdh <50341781+gkbdh@users.noreply.github.com>
2025-03-19 16:44:35 +0100

    Update config.h

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/config.h
@@ -26,6 +26,13 @@
 #    define NO_ACTION_ONESHOT
 #endif // __arm__
 
+const uint16_t PROGMEM test_combo1[] = {KC_A, KC_B, COMBO_END};
+const uint16_t PROGMEM test_combo2[] = {KC_C, KC_D, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(test_combo1, KC_ESC),
+    COMBO(test_combo2, LCTL(KC_Z)), // keycodes with modifiers are possible too!
+};
+
 /* Charybdis-specific features. */
 
 #ifdef POINTING_DEVICE_ENABLE
